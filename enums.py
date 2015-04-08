b25698ea6d97430262a43655523fd71aa43f43ed
Mislav Bradac <mislav.bradac@gmail.com>
2015-04-08 21:25:54 +0200

    Added enums for contests and organizers

---
+++ enums.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Organizer(Enum):
+    azoo = 'AZOO'
+    hsin = 'HSIN'
+    mioc = 'MIOC'
+    xfer = 'XFER'
+    zrs = 'ZRS'
+
+class ContestName(Enum):
+    skolsko = 'Skolsko Natjecanje'
+    zupanijsko = 'Zupanijsko Natjecanje'
+    drzavno = 'Drzavno Natjecanje'
+    hio = 'Hrvatska informaticka Olimpijada'
+    jhio = 'Juniorska Hrvatska Informatici Olimpijada'
+    izborne = 'Izborne Pripreme'
+    honi = 'Hrvatsko Otvoreno Natjecanje u Informatici'
+    studentsko = 'Natjecanje Timova Studenata Informaticara Hrvatskih Sveucilista'
+
